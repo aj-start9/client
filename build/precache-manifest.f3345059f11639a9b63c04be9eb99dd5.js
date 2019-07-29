008e1373a6c2bc80c5ca8e8c7e29121b62e93357
ankit9678 <ankit.jain@opscale.io>
2019-07-29 18:26:11 +0530

    code push

---
+++ build/precache-manifest.f3345059f11639a9b63c04be9eb99dd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe27c6501b2375bc9b66b944216ba090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19294a34c689bebbb949",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "07e299a81dbbd11c5f79",
+    "url": "/static/js/2.b6ce2cd9.chunk.js"
+  },
+  {
+    "revision": "19294a34c689bebbb949",
+    "url": "/static/js/main.fbcc942b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
